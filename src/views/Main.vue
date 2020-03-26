78947e1ff6f4d73f9e01457a233af64c63411b70
benjaminkent <benjamin.k.jehl@gmail.com>
2020-03-25 21:22:38 -0400

    get rid of unneeded components

---
+++ src/views/Main.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="main-container">
+    main page
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+
+export default Vue.extend({
+  setup() {
+    return {}
+  },
+})
+</script>
